09195ab4ac5e5be3b4a75da6390e128915cf0b8b
JDannald <jdannaldson@ewu.edu>
2024-04-13 21:19:53 -0700

    fUCK

--- src/html/table.py
+++ src/html/table.py
@@ -1,36 +1,30 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flask import render_template
 
 app = Flask(__name__)
 CORS(app)
 
+open = "open"
+close = "close"
+tableData = [
+    
+    [open,open,close],
+    [close,open,close],
+    [open,close,close]
+    
+]
+
+
 @app.route("/")
 def table():
-    
-    tableData = ""
-    
-    for x in range(0, 4):
-        #print <tr>
-        row = "<tr>" 
-        
-        
-        for y in range (0, 21):
-            
-            row += "<td>open</td>"
-        
-    
-        row += "</tr>"
-        tableData += row
             
 
-    return render_template('parking_page.html', tableData)
+    return render_template('parking_page.html', trial = tableData)
 
 
 
 if __name__ == "__main__":''
     
-app.run()
-
-
+app.run(debug = True)
     
